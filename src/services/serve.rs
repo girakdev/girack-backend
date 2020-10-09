714d97f626a0a3c3699118a0448c4d4198f7b8c2
itok01 <28337009+itok01@users.noreply.github.com>
2020-10-09 17:47:01 +0900

    Add allowed origin

--- src/services/serve.rs
+++ src/services/serve.rs
@@ -1,6 +1,7 @@
 use super::*;
 use crate::util::get_env;
-use actix_web::{middleware, App, HttpServer};
+use actix_cors::Cors;
+use actix_web::{http, middleware, App, HttpServer};
 
 /// Run HTTP server
 pub async fn run() -> std::io::Result<()> {
@@ -11,6 +12,17 @@ pub async fn run() -> std::io::Result<()> {
 
         App::new()
             .wrap(middleware::Logger::default())
+            .wrap(
+                Cors::new()
+                    .allowed_origin_fn(|req| {
+                        req.headers
+                            .get(http::header::ORIGIN)
+                            .map(http::HeaderValue::as_bytes)
+                            .filter(|b| b.ends_with(b".example.com"))
+                            .is_some()
+                    })
+                    .finish(),
+            )
             .service(get_message_handler)
             .service(post_message_handler)
     })
