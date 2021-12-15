b38796ce4d01e2aea7ccebe2f244a68275123b7b
whale-yay <ic171226@gmail.com>
2021-12-15 10:21:21 +0900

    update: add router

---
+++ app/router/routing.go
@@ -0,0 +1,38 @@
+package router
+
+import (
+  "github.com/gin-gonic/gin"
+  "app/controller"
+)
+
+func CreateRouter() *gin.Engine {
+  router := gin.Default()
+
+  girackRouter := router.Group("/girack")
+  {
+    v1 := girackRouter.Group("/v1")
+    {
+      users := v1.Group("/users")
+      {
+        users.POST("", controller.CreateUser)
+        users.POST(":id", controller.UpdateUser)
+        users.GET(":id", controller.GetUser)
+        users.GET("", controller.GetAllUser)
+        users.DELETE(":id", controller.DeleteUser)
+      }
+ /*     channels := v1.Group("/channels")
+      {
+        channels.POST("", controller.CreatChannel)
+        channels.POST("", controller.UpdateChannel)
+        channels.GET("", controller.GetChannel)
+        channels.GET("", controller.GetAllChannell)
+        channels.DELETE("", controller.DeleteChannel)
+      }
+  */
+    }
+  }
+
+  router.Run()
+
+  return router
+}
