d8399c8f362c4f2b884daad6860ad62f66259d7c
whale-yay <ic171226@gmail.com>
2021-12-15 11:35:31 +0900

    update: iroiro

--- app/main.go
+++ app/main.go
@@ -1,37 +1,12 @@
 package main
 
 import (
-  "github.com/gin-gonic/gin"
-  "app/controller"
+  "app/router"
   "app/db"
 )
+
 func main() {
   db.InitDB()
-  router := gin.Default()
-
-  girackRouter := router.Group("/girack")
-  {
-    v1 := girackRouter.Group("/v1")
-    {
-      users := v1.Group("/users")
-      {
-        users.POST("", controller.CreateUser)
-        users.POST(":id", controller.UpdateUser)
-        users.GET(":id", controller.GetUser)
-        users.GET("", controller.GetAllUser)
-        users.DELETE(":id", controller.DeleteUser)
-      }
-      channels := v1.Group("/channels")
-      {
-        channels.POST("", controller.CreateUser)
-        channels.POST("", controller.CreateUser)
-        channels.GET("", controller.CreateUser)
-        channels.GET("", controller.CreateUser)
-        channels.DELETE("", controller.DeleteUser)
-      }
-    }
-  }
-
-  router.Run()
+  router.CreateRouter().Run()
   db.CloseDB()
 }
