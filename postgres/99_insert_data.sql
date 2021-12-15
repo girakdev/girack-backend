141a16533f8e8a0ecc5add1709e1bd61538da2f8
whale-yay <ic171226@gmail.com>
2021-12-15 11:34:28 +0900

    modified: initial data

--- postgres/99_insert_data.sql
+++ postgres/99_insert_data.sql
@@ -1,3 +1,3 @@
-INSERT INTO users (email, name) VALUES ('testuser1@test.com', 'testuser1');
-INSERT INTO users (email, name) VALUES ('testuser2@test.com', 'testuser2');
-INSERT INTO users (email, name) VALUES ('testuser3@test.com', 'testuser3');
+INSERT INTO users (email, name) VALUES ('defaultuser1@xxx.com', 'hiroshi1');
+INSERT INTO users (email, name) VALUES ('defaultuser2@xxx.com', 'tanaka2');
+INSERT INTO users (email, name) VALUES ('defaultuser3@xxx.com', 'kirota3');
