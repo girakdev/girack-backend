9908a387f0a9fb8825aa6f346c528f58ac21b0ea
whale-yay <ic171226@gmail.com>
2021-12-15 13:14:05 +0900

    update: DB設定のハードコーディング回避

--- app/db/db.go
+++ app/db/db.go
@@ -1,13 +1,27 @@
 package db
 
 import (
+  "fmt"
+  "os"
+
   "database/sql"
   _ "github.com/lib/pq"
+
   "log"
 )
 
-var Db *sql.DB
-var err error
+var (
+  Db *sql.DB
+  err error
+)
+
+var (
+  schema = "host=%s user=%s password=%s dbname=%s sslmode=disable"
+  host     = "girack_db"
+  userName = os.Getenv("POSTGRES_USER")
+  password = os.Getenv("POSTGRES_PASSWORD")
+  dbName   = os.Getenv("POSTGRES_DB")
+)
 
 const (
   queryGetUser    = "SELECT email, name FROM users WHERE id=$1"
@@ -19,9 +33,7 @@ func logFatal(err error) {
 }
 
 func InitDB() {
-  // security hole
-  conf := "host=girack_db user=girack_user password=password dbname=girack_db sslmode=disable"
-
+  conf := fmt.Sprintf(schema, host, userName, password, dbName)
 
   Db, err = sql.Open("postgres", conf)
   logFatal(err)
