8584d818bfb1e1c2c778aceed2e9391ae0135150
whale-yay <ic171226@gmail.com>
2021-12-20 11:57:30 +0900

    db totonoeta

--- postgres/1_create.sql
+++ postgres/1_create.sql
@@ -1,11 +1,12 @@
 CREATE TABLE users (
   id SERIAL NOT NULL,
-  email VARCHAR(20) NOT NULL,
+  email TEXT NOT NULL,
   name VARCHAR(80) NOT NULL,
-  displayed_name VARCHAR(80) NOT NULL,
-  encrypted_password TEXT NOT NULL,
-  profile_picture_path TEXT NOT NULL,
+  displayed_name VARCHAR(80),
+  password TEXT NOT NULL,
+  profile_picture TEXT,
   created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+  updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
   PRIMARY KEY (id)
 );
 
@@ -13,8 +14,9 @@ CREATE TABLE channels (
   id SERIAL NOT NULL,
   name VARCHAR(100) NOT NULL,
   description VARCHAR(255),
-  created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
   dm_flag boolean NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+  updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
   PRIMARY KEY (id)
 );
 
@@ -23,6 +25,9 @@ CREATE TABLE messages (
   message TEXT NOT NULL,
   user_id int8 NOT NULL,
   channel_id int8 NOT NULL,
-  send_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
-  PRIMARY KEY (id)
+  created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+  updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) references users(id),
+  FOREIGN KEY (channel_id) references users(id)
 )
--- postgres/99_insert_data.sql
+++ postgres/99_insert_data.sql
@@ -1,3 +1,3 @@
-INSERT INTO users (email, name) VALUES ('defaultuser1@xxx.com', 'hiroshi1');
-INSERT INTO users (email, name) VALUES ('defaultuser2@xxx.com', 'tanaka2');
-INSERT INTO users (email, name) VALUES ('defaultuser3@xxx.com', 'kirota3');
+INSERT INTO users (email, name, password) VALUES ('defaultuser1@xxx.com', 'hiroshi1', 'pass1');
+INSERT INTO users (email, name, password) VALUES ('defaultuser2@xxx.com', 'tanaka2', 'pass2');
+INSERT INTO users (email, name, password) VALUES ('defaultuser3@xxx.com', 'kirota3', 'pas3');
