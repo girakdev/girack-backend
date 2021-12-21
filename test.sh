d2285db38f6b16644585ba0ae6ea4aa8f7d67ff4
whale-yay <ic171226@gmail.com>
2021-12-21 11:30:25 +0900

    delete cookie.txt

--- test.sh
+++
@@ -1,2 +0,0 @@
-curl -c cookie.txt -X POST -H "Content-Type: application/json" -d '{"email":"testtest@testmail.com", "name":"test tarou", "password": "paaaaassword"}' localhost:8080/girack/v1/register
-curl -c cookie.txt -X POST -H "Content-Type: application/json" -d '{"email":"testtest@testmail.com", "password": "paaaaassword"}' localhost:8080/girack/v1/login
