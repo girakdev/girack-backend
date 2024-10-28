0e63420dc00e5fe2e4d2fc18003fc7f51bbae18d
itok01 <ic171226@gmail.com>
2024-10-28 05:31:59 +0000

    add: mock

---
+++ infrastructure/datastore/channnel_test.go
@@ -0,0 +1,21 @@
+package datastore
+
+import (
+	"testing"
+)
+
+func TestChannelRepository_GetChannel(t *testing.T) {
+	// TODO: implement test
+}
+
+func TestChannelRepository_GetChannelList(t *testing.T) {
+	// TODO: implement test
+}
+
+func TestChannelRepository_CreateChannel(t *testing.T) {
+	// TODO: implement test
+}
+
+func TestChannelRepository_DeleteChannel(t *testing.T) {
+	// TODO: implement test
+}
